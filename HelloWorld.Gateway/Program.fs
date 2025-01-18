694a9de363f05c06ea612dc265c3d2eb383a74c8
bilinsky <yuri_bilinski@hotmail.com>
2025-01-17 22:25:56 -0300

    ocelot

--- HelloWorld.Gateway/Program.fs
+++ HelloWorld.Gateway/Program.fs
@@ -1,21 +1,24 @@
+
 open System
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.DependencyInjection
 open Microsoft.AspNetCore.Hosting
+open Ocelot.DependencyInjection
+open Ocelot.Middleware
 
 [<EntryPoint>]
 let main args =
     let builder = WebApplication.CreateBuilder(args)
 
-    // Configurar o Gateway para escutar na porta 80
+    // Configurar o Ocelot
     builder.WebHost.UseUrls("http://*:80") |> ignore
+    builder.Services.AddOcelot() |> ignore
 
     let app = builder.Build()
 
-    // Exemplo de rota padrão
-    app.MapGet("/", Func<string>(fun () -> "Hello, World from API!")) |> ignore
-
-    app.Run()
+    // Configurar o middleware do Ocelot
+    app.UseOcelot().Wait()
 
     0 // Exit code
 
