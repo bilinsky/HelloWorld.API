5c5cff1d346b41431074911857e72db9ac76059b
bilinsky <yuri_bilinski@hotmail.com>
2025-01-18 21:29:12 -0300

    ajuste no ocelot

--- HelloWorld.Gateway/Program.fs
+++ HelloWorld.Gateway/Program.fs
@@ -1,9 +1,8 @@
-
 open System
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.DependencyInjection
-open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.Logging
 open Ocelot.DependencyInjection
 open Ocelot.Middleware
 
@@ -11,14 +10,26 @@ open Ocelot.Middleware
 let main args =
     let builder = WebApplication.CreateBuilder(args)
 
+    // Configurar logs
+    builder.Logging.ClearProviders() |> ignore
+    builder.Logging.AddConsole() |> ignore
+
     // Configurar o Ocelot
-    builder.WebHost.UseUrls("http://*:80") |> ignore
     builder.Services.AddOcelot() |> ignore
 
     let app = builder.Build()
 
-    // Configurar o middleware do Ocelot
+    // Configurar URLs
+    app.Urls.Add("http://0.0.0.0:80")
+
+    // Middleware do Ocelot
     app.UseOcelot().Wait()
 
-    0 // Exit code
+    // Log de inicialização
+    let logger: ILogger<obj> = app.Services.GetRequiredService<ILogger<obj>>()
+    logger.LogInformation("Ocelot Gateway iniciado e aguardando conexões...")
 
+    // Executar o servidor
+    app.Run()
+
+    0 // Código de saída
